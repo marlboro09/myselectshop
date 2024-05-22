4c3831298ecce633e6ac3d1e313aac0963d32813
marlboro09 <taebong8989@gmail.com>
2024-05-22 23:21:06 +0900

    상품 페이징 및 정렬

--- src/main/java/com/sparta/myselectshop/controller/ProductController.java
+++ src/main/java/com/sparta/myselectshop/controller/ProductController.java
@@ -2,6 +2,7 @@ package com.sparta.myselectshop.controller;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sparta.myselectshop.dto.ProductMypriceRequestDto;
@@ -37,12 +39,13 @@ public class ProductController {
 	}
 
 	@GetMapping("/products")
-	public List<ProductResponseDto> getProducts(@AuthenticationPrincipal UserDetailsImpl userDetails) {
-		return productService.getProducts(userDetails.getUser());
+	public Page<ProductResponseDto> getProducts(
+		@RequestParam("page") int page,
+		@RequestParam("size") int size,
+		@RequestParam("sortBy") String sortBy,
+		@RequestParam("isAsc") boolean isAsc,
+		@AuthenticationPrincipal UserDetailsImpl userDetails) {
+		return productService.getProducts(userDetails.getUser(),page - 1, size, sortBy, isAsc);
 	}
 
-	@GetMapping("/admin/products")
-	public List<ProductResponseDto> getAllProducts() {
-        return productService.getAllProducts();
-    }
 }
--- src/main/java/com/sparta/myselectshop/service/ProductService.java
+++ src/main/java/com/sparta/myselectshop/service/ProductService.java
@@ -3,6 +3,10 @@ package com.sparta.myselectshop.service;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,6 +15,7 @@ import com.sparta.myselectshop.dto.ProductRequestDto;
 import com.sparta.myselectshop.dto.ProductResponseDto;
 import com.sparta.myselectshop.entity.Product;
 import com.sparta.myselectshop.entity.User;
+import com.sparta.myselectshop.entity.UserRoleEnum;
 import com.sparta.myselectshop.naver.dto.ItemDto;
 import com.sparta.myselectshop.repository.ProductRepository;
 
@@ -45,15 +50,22 @@ public class ProductService {
 		return new ProductResponseDto(product);
 	}
 
-	public List<ProductResponseDto> getProducts(User user) {
-		List<Product> productList = productRepository.findAllByUser(user);
-		List<ProductResponseDto> responseDtoList = new ArrayList<>();
+	public Page<ProductResponseDto> getProducts(User user, int page, int size, String sortBy, boolean isAsc) {
+		Sort.Direction direction = isAsc ? Sort.Direction.ASC : Sort.Direction.DESC;
+		Sort sort = Sort.by(direction, sortBy);
+		Pageable pageable = PageRequest.of(page, size, sort);
 
-		for (Product product : productList) {
-			responseDtoList.add(new ProductResponseDto(product));
+		UserRoleEnum userRoleEnum = user.getRole();
+
+		Page<Product> productList;
+
+		if(userRoleEnum == UserRoleEnum.USER) {
+			productList = productRepository.findAllByUser(user, pageable);
+		} else {
+			productList = productRepository.findAll(pageable);
 		}
 
-		return responseDtoList;
+		return productList.map(ProductResponseDto::new);
 	}
 
 	@Transactional
@@ -65,14 +77,4 @@ public class ProductService {
         product.updateByItemDto(itemDto);
 	}
 
-	public List<ProductResponseDto> getAllProducts() {
-		List<Product> productList = productRepository.findAll();
-		List<ProductResponseDto> responseDtoList = new ArrayList<>();
-
-		for (Product product : productList) {
-			responseDtoList.add(new ProductResponseDto(product));
-		}
-
-		return responseDtoList;
-	}
 }
