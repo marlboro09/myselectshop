c8813ab39c5c9cd87fe79e788a0670aee6433ce4
marlboro09 <taebong8989@gmail.com>
2024-05-22 18:07:51 +0900

    관심상품 조회 API

--- src/main/java/com/sparta/myselectshop/service/ProductService.java
+++ src/main/java/com/sparta/myselectshop/service/ProductService.java
@@ -1,5 +1,8 @@
 package com.sparta.myselectshop.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -38,5 +41,16 @@ public class ProductService {
 		product.update(requestDto);
 
 		return new ProductResponseDto(product);
+	}
+
+	public List<ProductResponseDto> getProducts() {
+		List<Product> productList = productRepository.findAll();
+		List<ProductResponseDto> responseDtoList = new ArrayList<>();
+
+		for (Product product : productList) {
+			responseDtoList.add(new ProductResponseDto(product));
+		}
+
+		return responseDtoList;
 	}
 }
