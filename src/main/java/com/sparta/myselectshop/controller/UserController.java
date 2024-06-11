ddbf17a6dcfa443abfa2e55945cd572b483e44d8
marlboro09 <taebong8989@gmail.com>
2024-06-11 20:16:11 +0900

    인가 코드로 access_token 가져오기 & 카카오 사용자 정보 가져오기 구현

--- src/main/java/com/sparta/myselectshop/controller/UserController.java
+++ src/main/java/com/sparta/myselectshop/controller/UserController.java
@@ -1,15 +1,7 @@
 package com.sparta.myselectshop.controller;
 
-import com.sparta.myselectshop.dto.SignupRequestDto;
-import com.sparta.myselectshop.dto.UserInfoDto;
-import com.sparta.myselectshop.entity.Folder;
-import com.sparta.myselectshop.entity.UserRoleEnum;
-import com.sparta.myselectshop.security.UserDetailsImpl;
-import com.sparta.myselectshop.service.FolderService;
-import com.sparta.myselectshop.service.UserService;
-import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import java.util.List;
+
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -18,9 +10,24 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.List;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.sparta.myselectshop.dto.SignupRequestDto;
+import com.sparta.myselectshop.dto.UserInfoDto;
+import com.sparta.myselectshop.entity.UserRoleEnum;
+import com.sparta.myselectshop.jwt.JwtUtil;
+import com.sparta.myselectshop.security.UserDetailsImpl;
+import com.sparta.myselectshop.service.FolderService;
+import com.sparta.myselectshop.service.KakaoService;
+import com.sparta.myselectshop.service.UserService;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Controller
@@ -30,6 +37,7 @@ public class UserController {
 
 	private final UserService userService;
 	private final FolderService folderService;
+	private final KakaoService kakaoService;
 
 	@GetMapping("/user/login-page")
 	public String loginPage() {
@@ -45,7 +53,7 @@ public class UserController {
 	public String signup(@Valid SignupRequestDto requestDto, BindingResult bindingResult) {
 		// Validation 예외처리
 		List<FieldError> fieldErrors = bindingResult.getFieldErrors();
-		if(fieldErrors.size() > 0) {
+		if (fieldErrors.size() > 0) {
 			for (FieldError fieldError : bindingResult.getFieldErrors()) {
 				log.error(fieldError.getField() + " 필드 : " + fieldError.getDefaultMessage());
 			}
@@ -74,5 +82,16 @@ public class UserController {
 		model.addAttribute("folders", folderService.getFolders(userDetails.getUser()));
 
 		return "index :: #fragment";
+	}
+
+	@GetMapping("/user/kakao/callback")
+	public String kakaoLogin(@RequestParam String code, HttpServletResponse response) throws JsonProcessingException {
+		String token = kakaoService.kakaoLogin(code);
+
+		Cookie cookie = new Cookie(JwtUtil.AUTHORIZATION_HEADER, token);
+		cookie.setPath("/");
+		response.addCookie(cookie);
+
+		return "redirect : /";
 	}
 }
